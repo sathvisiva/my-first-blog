f318da26f538a857f343ccc60dbfa138ca908879
sathvisiva <sathvisiva@gmail.com>
2015-11-20 00:35:20 +0530

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,30 @@
+print('Hello, Django girls!')
+if 3 < 2:
+    print('It works!')
+else:
+	print('else works')
+
+name = "sathvi"
+if name == "tinku":
+	print('name is tinku')
+elif name == "prashanth":
+	print('name is prashanth')
+else:
+	print('name is sathvi')
+
+def hi():
+	print('Hi')
+	print('Hello')
+hi()
+
+def hello(name):
+	print(name)
+hello("sathvi")
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+	print(name)
+
+for i in range(1, 6):
+    print(i)
+
